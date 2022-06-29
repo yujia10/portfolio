e91af3a4e3f7a04052efe83dd6f837ce9d329b0a
Yujia Gu <yujiagu1010@gmail.com>
2022-06-29 09:59:08 +0930

    create navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {CgWebsite} from 'react-icons/cg'
+import {BiMessageSquareDetail} from 'react-icons/bi'
+
 
 const Nav = () => {
+  const [active, setActive] = useState('#')
+
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={()=>setActive('#')} className={active === '#' && 'active'}><AiOutlineHome /></a>
+      <a href="#about" onClick={()=>setActive('#about')} className={active === '#about' && 'active'}><AiOutlineUser /></a>
+      <a href="#experience" onClick={()=>setActive('#experience')} className={active === '#experience' && 'active'}><BiBook /></a>
+      <a href="#portfolio" onClick={()=>setActive('#portfolio')} className={active === '#portfolio' && 'active'}><CgWebsite /></a>
+      <a href="#contact" onClick={()=>setActive('#contact')} className={active === '#contact' && 'active'}><BiMessageSquareDetail /></a>
+    </nav>
   )
 }
 
