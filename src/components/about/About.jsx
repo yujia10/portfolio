caea5629bf4200604891c82fc50d7d49e3196ebc
Yujia Gu <yujiagu1010@gmail.com>
2022-07-04 10:11:50 +0930

    change about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,21 +3,31 @@ import './about.css'
 import ME2 from '../../assets/me2.JPG'
 import {FaAward} from 'react-icons/fa'
 
-const aboutData = {
-  Experience: 'I have 8 years experience of website/software localization and also used to be a childcare teacher. I have been teaching myself front-end development for a while and recently just finished a web development bootcamp. ',
-  Projects: 'My recent projects include a car rental website and a childcare search engine (with Ruby on Rails), a note-taking app (with React.js) and a plant manager (with React.js)',
-  Hobbies: 'I enjoy cooking and baking with my family. In my spare time, I love playing piano and caring for my succulents'
-}
+const aboutData = [
+  {
+    id:1,
+    title:'Experience',
+    description:'I have 8 years experience of website/software localization and also used to be a childcare teacher. I have been teaching myself front-end development for a while and recently finished a web development bootcamp. ',
+  },
+  {
+    id:2,
+    title:'Projects',
+    description: 'My recent projects are built with Ruby on Rails and React.js, including a car rental website, a childcare search engine, a note-taking app and a plants manager.'
+  },
+  {
+    id:3,
+    title:'Hobbies',
+    description: 'I enjoy cooking and baking with my family. In my spare time, I love playing piano and caring for my succulents.'
+  }
+]
 
 function About() {
-  const [cardActive, setCardActive] = useState('false')
-  const handleClick = ()=>{
-    setCardActive(!cardActive)
-  }
+  const [value, setValue] = useState(0)
+
+  const {description} = aboutData[value]
 
   return (
    <section id='about'>
-
 
      <div className="container about-container">
        <div className="about-me">
@@ -27,24 +37,20 @@ function About() {
        </div>
 
        <div className="about-content">
-         <div className="about-cards">
-           <article className={cardActive ? 'about-card card-active' : 'about-card'} onClick={handleClick}>
-             <FaAward className="about-icon"/>
-             <h5>Experience</h5>
-           </article>
-           <article className= 'about-card'>
-           <FaAward className="about-icon"/>
-             <h5>Projects</h5>
-           </article>
-           <article className= 'about-card'>
-           <FaAward className="about-icon"/>
-             <h5>Hobbies</h5>
-           </article>
-         </div>
-
-         <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos, repellat, distinctio earum sint iure eligendi a rem beatae consectetur accusantium aut dolor atque dolores autem minima adipisci, placeat suscipit possimus!
-         </p>
+        <div className='about-cards'>
+        {aboutData.map((item,index)=>{
+         return (
+          <article className={`about-card ${index === value && 'card-active'}`}
+          key={item.id}
+          onClick = {()=>setValue(index)}>
+            <FaAward className="about-icon"/>
+            <h5>{item.title}</h5>
+          </article>
+          )
+          })
+        }
+        </div>
+        <p>{description}</p>
 
          <a href="#contact" className='btn btn-primary'>Let's talk</a>
        </div>
